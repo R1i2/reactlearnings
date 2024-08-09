279206b49f8effaef710739cbaba62c106c22ec3
Rishav Raj <52792420+R1i2@users.noreply.github.com>
2024-08-09 22:47:39 +0530

    Initial commit

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+// import * as util from "./util";
+// console.log(util.apiKey);
+// import { apiKey } from "./util";
+// console.log(apiKey);
+/**
+ * export default "SASKey";
+ */
+// import util from "./util";
+// console.log(util);
+/*
+ ** Multiple Imports
+ */
+import * as util from "./util";
+console.log(util.a);
+console.log(util.SASKey);
+console.log(util.b);
---
+++ scripts/util.js
@@ -0,0 +1,4 @@
+// export let apiKey = "SAS";
+export default "SASKey";
+export let a = 1;
+export let b = "z";
